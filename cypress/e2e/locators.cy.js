b05970aa9fa1443aba96a3b4f45be8fda4a46f1f
DİKMENA <alidikmende@gmail.com>
2023-09-29 12:57:34 +0200

    assertion types

--- cypress/e2e/locators.cy.js
+++ cypress/e2e/locators.cy.js
@@ -8,7 +8,7 @@ describe("Find or Get Elements by Using Different Locators", () => {
       })
 
       // Open Cypress | Set ".only"
-      it('Check different locators strategies', () => {
+      xit('Check different locators strategies', () => {
         // By CSS locator
         cy.get("input[name='username']").type("CydeoStudent");  // every statement creates an object to be interactived,, and next command makes operation to the ovject created at the previous statement.
         // attribute name and value 
@@ -34,4 +34,23 @@ describe("Find or Get Elements by Using Different Locators", () => {
         // if I want to use text: no xpath in cypress, but it is still possible with a different approcach ---> Login is text and button is tag name.
         cy.get('button').should('contain','Login').click();
       })
-    })
+      it('Check finding elements by traveling through DOM', () => {
+        // travel to find the login button: locate username box- go to parent form -then find button
+        cy.get('input[name="username"]').parents('form').find('button').should('contain', 'Login').click();
+
+      })
+      it.only('Check Different Type of Assertions'), () => {
+        // Cypress itself bundles assertions provided by Chai, Sinon and jQuery libraries
+        // Should Assertion: does the assertion directly on the object itself
+        cy.get('#wooden_spoon')
+        .should('contain', 'Login')
+        .and('have.class','btn btn-primary');
+        // expect assertion: creates a subject of our test, then you implement different actions
+        cy.get('#wooden_spoon').then((buttonElement) => {
+            expect(buttonElement).to.have.text('Login');
+            expect(buttonElement).to.have.class('btn btn-primary');
+        })
+
+
+      }  
+      })
