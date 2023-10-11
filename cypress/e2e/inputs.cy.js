9e259d971f4357a83e97b7252bb08abc1dda69e2
DİKMENA <alidikmende@gmail.com>
2023-10-11 12:25:13 +0200

    radio button check

--- cypress/e2e/inputs.cy.js
+++ cypress/e2e/inputs.cy.js
@@ -1,25 +1,49 @@
 /// <reference types="cypress" />
 
 describe('Imput Forms Tests', () => {
-     
-    beforeEach('Navigate to registration page',() => {
-      // run before each test case, beforeMethod in TestNG
-      cy.clearCookies();
-      cy.visit('/registration_form');
-    });
-     
-    it('Check different input box fields and verify', () => {
-      // fill out the form for username and other info
-      cy.get('input[name="firstname"]').type('Mike');
-      cy.get('input[name="lastname"]').type('Brown');
-      cy.get('input[name="username"]').type('CrazyHeart');
-      /**
-       * Math.rondom(): creates a number between 0-1 ~ 0.005678
-       * Math.floor :makes it a whole number
-       */
-      let email =`formtest${Math.floor(100000+Math.random()*900000)}@cydeo.com`;
-      cy.get('input[name="email"]').type(email);
-    });
-    
+  beforeEach('Navigate to registration page', () => {
+    // run before each test case, beforeMethod in TestNG
+    cy.clearCookies();
+    cy.visit('/registration_form');
   });
-  
+
+  it.skip('Check different input box fields and verify', () => {
+    // fill out the form for username and other info
+    cy.get('input[name="firstname"]').type('Mike');
+    cy.get('input[name="lastname"]').type('Brown');
+    cy.get('input[name="username"]').type('CrazyHeart');
+    /**
+     * Math.rondom(): creates a number between 0-1 ~ 0.005678
+     * Math.floor :makes it a whole number
+     */
+    const email = `formtest${Math.floor(100000 + Math.random() * 900000)}@cydeo.com`;
+    cy.get('input[name="email"]').type(email);
+    const password = `test${Math.floor(100000 + Math.random() * 900000)}`;
+    cy.get('input[name="password"]').type(password);
+    const phoneNumber = `555-000-${Math.floor(1000 + Math.random() * 9000)}`; // 4 digit extension number
+    cy.get('input[name="phone"]').type(phoneNumber);
+    cy.get('input[name="birthday"]').type('01/01/1999');
+  });
+  it('Check different radio button actions', () => {
+    cy.get('.radio')
+    .find('[type=radio]')
+    .then(radio => {
+      // get all radio buttons, select the first one and verify that it is checked
+      cy.wrap(radio).first().check().should('be.checked'); // cyress works in a chainable functions structure
+      /**
+       * radio : is Jquery element, cy.wrap(radio) : turns it into Cypress Object so that I can use cypress functions
+       * first() : selects first element
+       * check() : checks it out
+       * should(): verifys whatever I provide as parameter 'be.checked'
+       */
+
+       // get all radio buttons, select the second one and verify that it is checked and confirdmation label is visible
+       cy.wrap(radio).eq(1).check().should('be.checked');
+       cy.get('[data-bv-icon-for="gender"]').should('be.visible');  // common function used in tests
+
+       // Third radio button is NOT checked
+       cy.wrap(radio).eq(2).should('not.be.checked');
+
+    })
+  })
+});
--- cypress/e2e/locators.cy.js
+++ cypress/e2e/locators.cy.js
@@ -1,56 +1,55 @@
 /// <reference types="cypress" />
 
-describe("Find or Get Elements by Using Different Locators", () => {
-    beforeEach(() => {
-        // run before each test case, beforeMethod in TestNG
-        cy.clearCookies();
-        cy.visit('/login');
-      })
+describe('Find or Get Elements by Using Different Locators', () => {
+  beforeEach(() => {
+    // run before each test case, beforeMethod in TestNG
+    cy.clearCookies();
+    cy.visit('/login');
+  });
 
-      // Open Cypress | Set ".only"
-      xit('Check different locators strategies', () => {
-        // By CSS locator
-        cy.get("input[name='username']").type("CydeoStudent");  // every statement creates an object to be interactived,, and next command makes operation to the ovject created at the previous statement.
-        // attribute name and value 
-        // cy.get(':nth-child(1) > .large-6 > input').clear();  // clear what is typed
-        cy.get("[type='text']").clear();
+  // Open Cypress | Set ".only"
+  xit('Check different locators strategies', () => {
+    // By CSS locator
+    cy.get("input[name='username']").type('CydeoStudent'); // every statement creates an object to be interactived,, and next command makes operation to the ovject created at the previous statement.
+    // attribute name and value
+    // cy.get(':nth-child(1) > .large-6 > input').clear();  // clear what is typed
+    cy.get("[type='text']").clear();
 
-        // tagName
-        cy.get("input").each((item, index, list) => {
-            // assert the lengtht of the list is 2
-            expect(list).to.have.length(2);
-            expect(item).to.have.attr("type");
+    // tagName
+    cy.get('input').each((item, index, list) => {
+      // assert the lengtht of the list is 2
+      expect(list).to.have.length(2);
+      expect(item).to.have.attr('type');
+    });
+    // by attribute Name
+    cy.get('[type]');
 
-        })
-        // by attribute Name
-        cy.get('[type]');
+    // by className
+    cy.get('.btn.btn-primary'); // class attribute den önce nokta koy ve boşluk varsa sil ve nokta koy
 
-        // by className
-        cy.get('.btn.btn-primary');  // class attribute den önce nokta koy ve boşluk varsa sil ve nokta koy
+    // By id/*  */
+    cy.get('#wooden_spoon'); // id value'dan önce # konulur.
 
-        // By id/*  */
-        cy.get('#wooden_spoon');   // id value'dan önce # konulur.
-
-        // if I want to use text: no xpath in cypress, but it is still possible with a different approcach ---> Login is text and button is tag name.
-        cy.get('button').should('contain','Login').click();
-      })
-      it('Check finding elements by traveling through DOM', () => {
-        // travel to find the login button: locate username box- go to parent form -then find button
-        cy.get('input[name="username"]').parents('form').find('button').should('contain', 'Login').click();
-
-      })
-      it.only('Check Different Type of Assertions'), () => {
-        // Cypress itself bundles assertions provided by Chai, Sinon and jQuery libraries
-        // Should Assertion: does the assertion directly on the object itself
-        cy.get('#wooden_spoon')
-        .should('contain', 'Login')
-        .and('have.class','btn btn-primary');
-        // expect assertion: creates a subject of our test, then you implement different actions
-        cy.get('#wooden_spoon').then((buttonElement) => {
-            expect(buttonElement).to.have.text('Login');
-            expect(buttonElement).to.have.class('btn btn-primary');
-        })
-
-
-      }  
-      })
+    // if I want to use text: no xpath in cypress, but it is still possible with a different approcach ---> Login is text and button is tag name.
+    cy.get('button').should('contain', 'Login').click();
+  });
+  it('Check finding elements by traveling through DOM', () => {
+    // travel to find the login button: locate username box- go to parent form -then find button
+    cy.get('input[name="username"]')
+      .parents('form')
+      .find('button')
+      .should('contain', 'Login')
+      .click();
+  });
+  it.only('Check Different Type of Assertions'),
+    () => {
+      // Cypress itself bundles assertions provided by Chai, Sinon and jQuery libraries
+      // Should Assertion: does the assertion directly on the object itself
+      cy.get('#wooden_spoon').should('contain', 'Login').and('have.class', 'btn btn-primary');
+      // expect assertion: creates a subject of our test, then you implement different actions
+      cy.get('#wooden_spoon').then((buttonElement) => {
+        expect(buttonElement).to.have.text('Login');
+        expect(buttonElement).to.have.class('btn btn-primary');
+      });
+    };
+});
